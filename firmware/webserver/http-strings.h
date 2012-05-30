704fb1fa87000ecae87c041889e227d4d55d8877
Justin Huff <jjhuff@mspin.net>
2012-05-29 21:25:52 -0700

    add webserver

---
+++ firmware/webserver/http-strings.h
@@ -0,0 +1,5 @@
+extern const char http_413_html[10];
+extern const char http_referer[10];
+extern const char http_header_200[16];
+extern const char http_header_404[23];
+extern const char http_server[50];
